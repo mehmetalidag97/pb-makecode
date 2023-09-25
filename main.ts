5ad73114f01cc13ce04854e48d8c3e0ed56277ef
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-09-25 08:43:32 +0300

    add buttons

--- main.ts
+++ main.ts
@@ -1,35 +1,3 @@
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.B)) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-    } else if (input.buttonIsPressed(Button.A)) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-    } else if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
-        basic.showLeds(`
-            # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
+	
 })
