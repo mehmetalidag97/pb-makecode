66aa7a222a6ef02405465caf125a5048660d8e1b
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-09-25 08:40:25 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,34 @@
+def on_forever():
+    if input.button_is_pressed(Button.B):
+        basic.show_leds("""
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            """)
+    elif input.button_is_pressed(Button.A):
+        basic.show_leds("""
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            """)
+    elif input.button_is_pressed(Button.A) and input.button_is_pressed(Button.B):
+        basic.show_leds("""
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            """)
+    else:
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,35 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
